506d741d6003c8c1c83cd9cc98752a5d87426e57
AlexeyChudinoff <201900@mail.ru>
2024-09-12 17:34:11 +0700

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Привет мир");
-       System.out.println("Hello World");
+        System.out.println("Сдлано Домашнее задание номер 1");
+        //System.out.println("Привет мир");
+       //System.out.println("Hello World");
     }
 }
